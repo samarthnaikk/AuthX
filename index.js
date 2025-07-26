7a629b0392190a3d3873c00a2c469b27d6d546ff
Samarth Naik <samarthnaik@Samarths-MacBook-Air.local>
2025-07-26 15:26:40 +0530

    added basic ui

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+const path = require('path');
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
@@ -27,6 +28,15 @@ app.use(limiter);
 
 // Routes
 app.use('/api/auth', authRoutes);
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/authx', {
